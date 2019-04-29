7c7e43a74ebfb56e296bda6a464f53c27e504b01
iftekharbhuiyan <ib.nadim@gmail.com>
2019-04-29 05:42:17 -0400

    JavaScript File

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', function (e) {
+	e.preventDefault();
+	
+	// data displaying div
+	var display = document.getElementById('weather');
+	
+	// city id for new york from open weather
+	const city_id = '5128581';
+	
+	// app id from open weather
+	const app_id = 'xxxxxxxxxxxxxxxxxxxxxx';
+	
+	// ajax request
+	const xhr = new XMLHttpRequest();
+	
+	// deal with a response
+	xhr.onload = function() {
+		// do whatever you want with your response
+		// parse json response
+		// var data = JSON.parse(this.response);
+	};
+	
+	// unsuccessful request
+	xhr.onerror = function () {
+		console.log('Something went wrong!');
+	};
+	
+	// initialize the request
+	xhr.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id='+city_id+'&appid='+app_id, true);
+	
+	// sends request
+	xhr.send();
+});
